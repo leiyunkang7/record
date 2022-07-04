96fd2bd6d663db45f452bd0d10900af8383e6b4c
leiyunkang <leiyunkang7@outlook.com>
2022-07-04 23:09:33 +0800

    添加vue-router

---
+++ src/routers/router.ts
@@ -0,0 +1,7 @@
+import VueRouter from 'vue-router'
+
+export const router = VueRouter.createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: VueRouter.createWebHashHistory(),
+  routes:[], // `routes: routes` 的缩写
+});
